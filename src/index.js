c90ef69f81cb1c11a102cbf5252cfd8a949bbe54
Mikhail Dunaev <mih.dunaev@gmail.com>
2024-11-19 20:03:19 +0100

    Ready. Set. GO!

---
+++ src/index.js
@@ -0,0 +1,71 @@
+import {Telegraf} from "telegraf";
+import 'dotenv/config';
+import fs from 'fs';
+
+const admins = process.env.USERS.split(',');
+const activated_file = process.env.ACTIVATED_FILE;
+let activated = [];
+
+function isUserInList(user) {
+    return admins.includes(user);
+}
+
+function validateUser(ctx) {
+    if (ctx.message.from.username) {
+        return isUserInList(ctx.message.from.username);
+    } else if (ctx.message.from.id) {
+        return isUserInList(ctx.message.from.id);
+    }
+
+    return false;
+}
+
+function main() {
+    const bot = new Telegraf(process.env.BOT_TOKEN);
+    if (fs.existsSync(activated_file)) {
+        activated = JSON.parse(fs.readFileSync(activated_file));
+    }
+    
+    bot.start((ctx) => ctx.reply('Привет, я бот для отправки анонимных сообщений! Отправь мне сообщение и я его перешлю получателю анонимно!'));
+    bot.help((ctx) => {
+        if (!validateUser(ctx)) {
+            ctx.reply('Отправь мне сообщение и я его перешлю получателю анонимно!')
+        } else {
+            ctx.reply('Отправь мне сообщение и я его перешлю получателю анонимно!\n\n' +
+                'Команды:\n' +
+                '/activate - активировать аккаунт админа\n');
+        }
+    });
+    
+    bot.command('activate', (ctx) => {
+        if (!validateUser(ctx)) {
+            return ctx.reply('Вам не разрешено использовать этого бота!');
+        }
+        ctx.reply(`Ваш аккаунт активирован!`);
+        activated.push(ctx.chat.id);
+        fs.writeFileSync(activated_file, JSON.stringify(activated));
+    });
+    
+    bot.on('message', async (ctx) => {
+        const message = ctx.message.text;
+        const attachment = ctx.message.photo || ctx.message.document || ctx.message.video || ctx.message.voice || ctx.message.audio || ctx.message.sticker;
+        let username = "Аноним";
+        if (ctx.message.from.username) {
+            username = ctx.message.from.username;
+        } else {
+            username = ctx.message.from.first_name + " " + ctx.message.from.last_name + " (id:" + ctx.message.from.id + ")";
+        }
+        
+        for (let chat_id of activated) {
+            await bot.telegram.sendMessage(chat_id, "Новое сообщение от пользователя @" + username);
+            await bot.telegram.sendMessage(chat_id, message);
+            if (attachment) {
+                await bot.telegram.sendCopy(chat_id, attachment);
+            }
+        }
+    });
+
+    bot.launch();
+}
+
+main();
